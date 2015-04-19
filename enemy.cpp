db4654ebd0ccf24fc2fd159950ed3ad768e7535b
raatmarien <raatmarien@users.noreply.github.com>
2015-04-19 20:13:32 +0200

    Add base enemy

---
+++ enemy.cpp
@@ -0,0 +1,83 @@
+#include "enemy.h"
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+#include <iostream>
+
+using namespace sf;
+
+void Enemy::initialize(Vector2f position
+                       , Texture enemyTexture
+                       , Texture nBulletTexture
+                       , b2World* nWorld
+                       , int nScale) {
+    int size = 50;
+    setPosition(position);
+    texture = enemyTexture;
+    alive = true;
+
+    lifes = 3;
+
+    sprite.setTexture(enemyTexture);
+    // vertices.setPrimitiveType(Quads);
+    // vertices.resize(4);
+
+    // vertices[2].position = Vector2f(0,0);
+    // vertices[0].position = Vector2f(size,0);
+    // vertices[1].position = Vector2f(size,size);
+    // vertices[3].position = Vector2f(0,size);
+
+    // vertices[2].texCoords = Vector2f(0,0);
+    // vertices[0].texCoords = Vector2f(size,0);
+    // vertices[1].texCoords = Vector2f(size,size);
+    // vertices[3].texCoords = Vector2f(0,size);
+    // origin?
+    bulletTexture = nBulletTexture;
+    world = nWorld;
+    scale = nScale;
+
+    // Setup body physics
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_staticBody;
+    bodyDef.position.Set((position.x + (size / 2))
+                         / scale
+                         , (position.y + (size / 2))
+                         / scale);
+    bodyDef.linearDamping = 2.0f;
+    bodyDef.angularDamping = 2.0f;
+    body = world->CreateBody(&bodyDef);
+
+    b2PolygonShape box;
+    box.SetAsBox(((float) (size)) / (2 * scale)
+                 , ((float) (size)) / (2 * scale));
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &box;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 1.0f;
+
+    body->CreateFixture(&fixtureDef);
+}
+
+void Enemy::update() {
+    b2Vec2 newPosition = body->GetPosition();
+    float angle = body->GetAngle();
+    setPosition(newPosition.x * scale
+                , newPosition.y * scale);
+    setRotation(angle * toDegreesMultiple);
+}
+
+void Enemy::hit() {
+    std::cout << lifes--;
+}
+
+bool Enemy::isAlive() {
+    return alive;
+}
+
+void Enemy::draw(RenderTarget& target
+                     , RenderStates states) const {
+    if (alive) {
+        states.transform *= getTransform();
+        states.texture = &texture;
+        target.draw(sprite, states);
+    }
+}
--- enemy.h
+++ enemy.h
@@ -1,41 +1,52 @@
+#pragma once
 #include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
 #include <vector>
 
 using namespace sf;
 
-class Bullet : public Drawable
-      , public Transformable {
-public:
-    Bullet(Vector2f startPosition
-           , Vector2f startSpeed
-           , b2World* world
-           , Texture texture
-           , int nScale);
-    void update();
-private:
-    int scale;
-    b2Body* body;
-    Sprite sprite;
-    virtual void draw(RenderTarget& target
-                      , RenderStates states);
-};
+/* class Bullet : public Drawable */
+/*       , public Transformable { */
+/* public: */
+/*     void initialize(Vector2f startPosition */
+/*                     , Vector2f startSpeed */
+/*                     , b2World* world */
+/*                     , Texture texture */
+/*                     , int nScale); */
+/*     void update(); */
+/* private: */
+/*     int scale; */
+/*     b2Body* body; */
+/*     Sprite sprite; */
+/*     virtual void draw(RenderTarget& target */
+/*                       , RenderStates states); */
+/* }; */
 
 class Enemy : public Drawable
-    , public Transformable {
+            , public Transformable {
 public:
-    initialize(Vector2f position
-               , Texture enemyTexture
-               , Texture nBulletTexture
-               , b2World* world
-               , int nScale);
-    update();
+    void initialize(Vector2f position
+                    , Texture enemyTexture
+                    , Texture nBulletTexture
+                    , b2World* nWorld
+                    , int nScale);
+    void update();
+    void hit();
+    bool isAlive();
 private:
+    int lifes;
+    bool alive;
     int scale;
-    Texture bulletTexture;
+    Texture texture
+        , bulletTexture;
     Sprite sprite;
+    /* VertexArray vertices; */
     b2Body* body;
     b2World* world;
-    Vector<Bullet> bullets;
+    /* std::vector<Bullet> bullets; */
     virtual void draw(RenderTarget& target
-                      , RenderStates states);
+                      , RenderStates states) const;
+
+    const static float toDegreesMultiple
+        = 57.2957795131;
 };
